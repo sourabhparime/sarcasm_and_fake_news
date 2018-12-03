5f6687d6a5b8c3cefeb602769636ee5c2f712171
sourabhparime <psourabh92@gmail.com>
2018-12-02 18:54:22 -0600

    NB

--- exec/Master.py
+++ exec/Master.py
@@ -1,12 +1,12 @@
 import pandas as pd 
 import numpy as np
 import codecs
-import pre 
-
+import pre, NB
+from sklearn.model_selection import train_test_split
 
 # Read the file and create a dict of tweets
-def read(path = '../data/Ano.xlsx'):
-    df = pd.read_excel(path, 'ElectionDay DATA')
+def read(path = '../data/Annnotated_Copy.xlsx'):
+    df = pd.read_excel(path, 'Annotated')
     tweets = df[['tweet_id', 'text', 'is_sarcasm']]
     return tweets
 
@@ -21,3 +21,23 @@ def clean_frame(tweets):
 cleaned_df = clean_frame(read())
 # save the cleaned df
 cleaned_df.to_csv("cleaned.csv")
+X = cleaned_df['text']
+y = cleaned_df['is_sarcasm']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20,
+ random_state=42)
+
+train_data = pd.concat([X_train, y_train], axis=1).reset_index()
+
+
+sar_tweets = []
+nonosar_tweets = []
+for index, row in train_data.iterrows():
+    if row['is_sarcasm'] == True:
+        sar_tweets.append((row['text'], 'sarcastic'))
+    else:
+        sar_tweets.append((row['text'], 'not_sarcastic'))
+# call naive bayes model and print classification report
+print(NB.get_accuracy(sar_tweets,nonosar_tweets, X_test ))
+ 
+
+
---
+++ exec/NB.py
@@ -0,0 +1,80 @@
+from nltk import bigrams
+from nltk import NaiveBayesClassifier
+from nltk import FreqDist
+import nltk
+
+def get_accuracy(sar, non_sar, test_tweets):
+        
+    def get_bigram_features(sar, non_sar):
+        bigram_feature_vectors = []
+        for tweet, tone in sar:
+            temp = []
+            
+            for item in bigrams(tweet.split()):
+                temp.append(item)
+            bigram_feature_vectors.append((temp, tone))
+
+        for tweet, tone in non_sar:
+            temp = []
+            #print(tweet)
+            for item in bigrams(tweet.split()):
+                temp.append(item)
+            bigram_feature_vectors.append((temp, tone))
+        return bigram_feature_vectors
+
+    def get_unigram_features(sar, non_sar):
+        bigram_feature_vectors = []
+        for tweet, tone in sar:
+            temp = []
+            #print(tweet)
+            temp.extend(tweet.split(" "))
+            bigram_feature_vectors.append((temp, tone))
+
+        for tweet, tone in non_sar:
+            temp = []
+            #print(tweet)
+            temp.extend(tweet.split(" "))
+            bigram_feature_vectors.append((temp, tone))
+        return bigram_feature_vectors
+
+
+    def get_word_features(bigram_feature_vectors):
+        # get all words
+        all_bigrams = []
+        for (bigrams, tone) in bigram_feature_vectors:
+            all_bigrams.extend(bigrams)
+        
+        wordlist = FreqDist(all_bigrams)
+        word_features  = wordlist.keys()
+        return word_features
+
+    def extract_feature(doc):
+        doc_words = set(doc)
+        features = {}
+        for word in word_features:
+            #print(word)
+            features['contains('+str(word)+')'] = (word in doc_words)
+        return features
+
+    bigram_feature_vectors = get_bigram_features(sar, non_sar)
+    #bigram_feature_vectors = get_unigram_features(sar, non_sar)
+    #print(bigram_feature_vectors)
+    word_features = get_word_features(bigram_feature_vectors)
+    print(sar)
+    training_set = nltk.classify.apply_features(extract_feature, bigram_feature_vectors)
+
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
+    print(classifier.show_most_informative_features(32))
+    print(classifier.classify(extract_feature("tell us more about how sexism is wrong gopdebate".split())))
+
+
+
+
+
+
+
+
+
+
+
+
