85466a057bc83b4fc447174ea41e51dee92786f2
sourabhparime <psourabh92@gmail.com>
2018-12-08 02:10:47 -0600

    intesifiers_phase1

---
+++ exec/Phase_1/intensifiers.py
@@ -0,0 +1,19 @@
+strong_affirmatives = ["yes", "yeah", "always", "all", "any", "every", "everybody", "everywhere", "ever"]
+
+strong_negations = ["no", "not", "never", "none" "n't", "nothing", "neither", "nobody", "nowhere"]
+
+punctuation = ["?", "!", "..."]
+
+interjections = ["oh", "hey", "wow", "aha", "aham", "aw", "bam", "blah", "bingo", "boo", "bravo",
+                 "cheers", "congratulations", "congrats", "duh", "eh", "gee", "gosh", "hey", "hmm",
+                 "huh", "hurray", "oh", "oh dear", "oh my", "oh well", "oops", "ouch", "ow", "phew",
+                 "shh", "uh", "uh-huh", "mhm", "ugh", "well", "wow", "woah", "yeah", "yep", "yikes", "yo"]
+
+intensifiers = ["amazingly", "astoundingly", "awful", "bare", "bloody", "crazy", "dreadfully",
+                "colossally", "especially", "exceptionally", "excessively", "extremely",
+                "extraordinarily", "fantastically", "frightfully", "fucking", "fully", "hella",
+                "holy", "incredibly", "insanely", "literally", "mightily", "moderately", "most",
+                "outrageously", "phenomenally", "precious", "quite", "radically", "rather",
+                "really", "remarkably", "right", "sick", "strikingly", "super", "supremely",
+                "surprisingly", "terribly", "terrifically", "too", "totally", "uncommonly",
+                "unusually", "veritable", "very", "wicked"]
